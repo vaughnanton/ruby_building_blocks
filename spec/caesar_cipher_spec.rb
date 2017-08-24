40c923f3c67af5cae4131edcd2721fa5c34190f1
Vaughn Antonyan <vaughnanton@gmail.com>
2017-08-23 17:59:43 -0700

    Rspec file complete

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,1 +1,16 @@
-#initialize spec file
+require './spec_helper'
+
+describe "cipher" do
+
+  it "translates a single lowercase word" do
+    cipher("hi", 1).should == "ij"
+  end
+
+  it "translates a single uppercase word" do
+    cipher("HELLO", 1).should == "IFMMP"
+  end
+
+  it "doesn't translate if shift is 0" do
+    cipher("hello", 0).should == "hello"
+  end
+end
